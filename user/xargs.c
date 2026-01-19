aa6f9d32e6be7304590dff648f322fd70d1613c1
ji096929 <2455742327@qq.com>
2026-01-19 15:15:17 +0800

    完成xargs

--- user/xargs.c
+++ user/xargs.c
@@ -1,42 +1,57 @@
+/*
+1 解析原生传入的命令行参数（命令和参数）
+2 从标准输入中逐行读取
+3 将读取的解析为参数
+4 将读取的参数追加到命令参数后面
+5 用fork和exec执行命令
+6 父进程等待子进程完成
+*/
 #include "kernel/types.h"
+
+#include "kernel/fs.h"
+#include "kernel/param.h"
+
 #include "user/user.h"
+#define MAXSIZE 256
 
-void xargs(char *argv[])
-{
-  char buf[512];
-  char *new_argv[10];
-  int i = 0;
-  while(argv[i] != 0)
-  {
-    new_argv[i] = argv[i];
-    i++;
-  }
-  int num = i;
-  int j = 0;
-  char c;
-  while(read(0,&c,1)>0)
-  {
-    if(c=='\n')
-    {
-      buf[i] = 0;
-      
-    }
-    else
-    {
-        buf[i++]=c;
-    }
-  }
-}
+int main(int argc, char *argv[]) {
 
-int main(int argc,char *argv[])
-{
-    if (argc < 2) {
-        fprintf(2, "Usage: xargs <command>\n");
+  if (argc < 2) {
+    exit(1);
+  }
+  char *xagrv[MAXARG];
+  int xagrc = 0;
+  sleep(10);
+  // 1
+  for (int i = 1; i < argc; i++) {
+    xagrv[xagrc++] = argv[i];
+  }
+
+  // 2
+  char buf[MAXSIZE];
+  read(0, buf, MAXSIZE);
+
+  // 3
+  char *p = buf;
+  for (int i = 0; i < MAXSIZE; i++) {
+    if (buf[i] == '\n') {
+
+      int pid = fork();
+      // 5
+      if (pid > 0) {
+        p=&buf[i+1];
+        wait(0);
+      } else {
+        // 4
+        buf[i] = 0;
+        xagrv[xagrc] = p;
+        xagrc++;
+        xagrv[xagrc] = 0;
+        exec(xagrv[0], xagrv);
         exit(1);
+      }
     }
-    else
-    {
-        xargs(argv+1);
-    }
-    exit(0);
+  }
+  exit(0);
+  return 0;
 }
