e1836e2cac2ddb99d10898b5a3c4d6a89296f56c
ji096929 <2455742327@qq.com>
2026-02-28 19:01:34 +0800

    1

--- kernel/trap.c
+++ kernel/trap.c
@@ -77,8 +77,17 @@ usertrap(void)
     exit(-1);
 
   // give up the CPU if this is a timer interrupt.
-  if(which_dev == 2)
+  if (which_dev == 2) {
+    if (p->alarm_ticks > 0) {
+      p->ticks_count++;
+      if (p->ticks_count == p->alarm_ticks) {
+        p->trapframe->epc=p->alarm_handler;
+        }
+      }
+    
     yield();
+    }
+    
 
   usertrapret();
 }
