b42fd01bc1c07087bb8d109285850b25c966f4b9
ji096929 <2455742327@qq.com>
2026-02-28 09:30:43 +0800

    实现部分

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -96,3 +96,21 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64 sys_sigalarm(void) {
+  int ticks;
+  uint64 handler;
+  if(argint(0, &ticks) < 0)
+    return -1;
+  if(argaddr(1, &handler) < 0)
+    return -1;
+  myproc()->alarm_ticks = ticks;
+  myproc()->alarm_handler = handler;
+  return 0;
+}
+
+uint64
+sys_sigreturn(void)
+{
+  return 0;
+}
